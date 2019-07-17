4416f0ff3cb8c266ddf9341feb5b8b95cee1e01f
yangsijie666 <490601519@qq.com>
2019-07-17 09:46:34 +0800

    删除无用代码

--- dubbo-consumer/src/main/java/com/dubbo/consumer/Consumer.java
+++ dubbo-consumer/src/main/java/com/dubbo/consumer/Consumer.java
@@ -6,22 +6,36 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Consumer {
     public static void main(String[] args) {
-
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("consumer.xml");
-
-        context.start();
         System.out.println("consumer start");
-        for (int i = 0; i <= 20; ++i) {
-            UserService userService = (UserService) context.getBean("userService");
-            System.out.println("consumer");
-            System.out.println(userService.getUserInfo(1));
-        }
 
-        for (int i = 0; i <= 20; ++i) {
-            CompanyService companyService = (CompanyService) context.getBean("companyService");
-            System.out.println("consumer");
-            System.out.println(companyService.getCompanyInfo());
+        while (true) {
+            try {
+                ClassPathXmlApplicationContext context =
+                        new ClassPathXmlApplicationContext("consumer.xml");
+
+                context.start();
+
+
+                for (int i = 0; i <= 20; ++i) {
+                    UserService userService = (UserService) context.getBean("userService");
+                    System.out.println(userService.getUserInfo(1));
+                }
+
+                for (int i = 0; i <= 20; ++i) {
+                    CompanyService companyService = (CompanyService) context.getBean("companyService");
+                    System.out.println(companyService.getCompanyInfo());
+                }
+
+                break;
+            } catch (com.alibaba.dubbo.rpc.RpcException e) {
+                try {
+                    Thread.sleep(1000);
+                    System.out.println("retry...");
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+
         }
     }
 }
